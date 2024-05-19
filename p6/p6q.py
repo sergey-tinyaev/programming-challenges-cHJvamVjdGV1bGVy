da40bd30713f46423061508719b12f7b336b3a9f
Sergey Tinyaev <t_serg@yahoo.com>
2024-05-19 18:03:11 -0500

    Refactor p6q to improve readability.

--- p6/p6q.py
+++ p6/p6q.py
@@ -3,4 +3,11 @@ def solve(n):
     and sum of squares of elements of arithmetic progression [1, 2, 3, ..., n]."""
     # Time complexity: O(1)
     # Space complexity: O(1)
-    return (n * (n + 1) // 2) ** 2 - n * (n + 1) * (2 * n + 1) // 6
+
+    # https://en.wikipedia.org/wiki/Triangular_number
+    square_of_sum = (n * (n + 1) // 2) ** 2
+
+    # https://en.wikipedia.org/wiki/Square_pyramidal_number
+    sum_of_squares = n * (n + 1) * (2 * n + 1) // 6
+
+    return square_of_sum - sum_of_squares
