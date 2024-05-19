4ce5b8d06b9cf29c1b9153d355d2bf1580cf07d2
Sergey Tinyaev <t_serg@yahoo.com>
2024-05-18 23:59:24 -0500

    Add description to solution p2q.

--- p2/p2q.py
+++ p2/p2q.py
@@ -1,8 +1,17 @@
+LIMIT = 4_000_000
+
+
 def solve():
-    r, a, b = 0, 1, 2
-    while b <= 4_000_000:
+    """Find answer to problem 2 by directly finding all valid Fibonacci terms.
+
+    Running time: O(LIMIT). The result comes from O(1.6 ^ N) for Fibonacci
+    sequence, where N ~ log(LIMIT, base=1.6).
+    Extra memory: O(1)
+    """
+    result, a, b = 0, 1, 2
+    while b <= LIMIT:
         if b % 2 == 0:
-            r += b
+            result += b
         a, b = b, a + b
 
-    return r
+    return result
