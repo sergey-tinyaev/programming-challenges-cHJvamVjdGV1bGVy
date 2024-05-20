f6523f8b542e204f225de4be68ade6cda060bf54
Sergey Tinyaev <t_serg@yahoo.com>
2024-05-20 13:19:51 -0500

    Add a solution for problem 13.

---
+++ p13/p13q.py
@@ -0,0 +1,30 @@
+def advance(x):
+    """Return next element in Collatz sequence."""
+    # Time complexity: O(1)
+    # Extra space complexity: O(1)
+    return (x // 2) if x % 2 == 0 else (3 * x + 1)
+
+
+def walk(x, cache):
+    """Return length of Collatz chain for a given starting number."""
+    # Time complexity: O(?) - unable to correctly deduce complexity.
+    # Extra space complexity: O(?) - unable to correctly deduce complexity.
+    if x not in cache:
+        cache[x] = 1 + walk(advance(x), cache)
+    return cache[x]
+
+
+def solve(n):
+    """Return starting number in [1..n] range that produces the longest
+    Collatz chain."""
+    # Time complexity: O(n * ?) - unable to correctly deduce complexity.
+    # Extra space complexity: O(?) - unable to correctly deduce complexity.
+    cache = {1: 1}
+
+    max_chain_count, result = 1, 1
+    for number in range(2, n + 1):
+        chain_count = walk(number, cache)
+        if chain_count > max_chain_count:
+            max_chain_count, result = chain_count, number
+
+    return result
