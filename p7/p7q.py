de4ce2b1d9bc5208e1a606afddeaf0b6ea9b34fc
Sergey Tinyaev <t_serg@yahoo.com>
2024-05-19 19:08:11 -0500

    Document p7q solution.

--- p7/p7q.py
+++ p7/p7q.py
@@ -1,19 +1,24 @@
 # Upper limit was taken from https://en.wikipedia.org/wiki/Prime-counting_function.
-MAX_PRIMES = 10**6
+MAX_NUMBER = 10**6
+MAX_SIEVE_SIZE = (MAX_NUMBER - 1) // 2
 
 
 def solve(n):
+    """Return n-th prime number below 1_000_000."""
+    # Time complexity: O(n).
+    # Space complexity: O(1) - a big constant is still a constant.
     if n == 1:
         return 2
 
-    x, sieve = 1, [True] * MAX_PRIMES
-    for i in range(MAX_PRIMES):
+    if n > 78498:
+        raise ValueError("n should be less than or equal to 78498.")
+
+    # See https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+    x, sieve = 1, [True] * MAX_SIEVE_SIZE
+    for i in range(MAX_SIEVE_SIZE):
         if sieve[i]:
             x += 1
             if x == n:
                 return 2 * i + 3
-
-            for j in range(2 * i * (i + 3) + 3, MAX_PRIMES, 2 * i + 3):
+            for j in range(2 * i * (i + 3) + 3, MAX_SIEVE_SIZE, 2 * i + 3):
                 sieve[j] = False
-
-    raise ValueError("the value is out of range")
