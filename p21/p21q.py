a8dd29e5741e6c61766830e75bb8cc7b05d2658f
Sergey Tinyaev <t_serg@yahoo.com>
2024-05-20 20:17:32 -0500

    Improve complexity analysis of p21q.

--- p21/p21q.py
+++ p21/p21q.py
@@ -18,6 +18,8 @@ def gen_primes(n: int) -> list[int]:
 
 def sum_of_proper_divisors(n: int, primes: list[int]) -> int:
     """Return sum of proper divisors of n."""
+    # Time complexity: O(n * sqrt(n) / log(n)).
+    # Extra space complexity: O(1).
     if n < 2:
         return 0
 
@@ -42,13 +44,13 @@ def sum_of_proper_divisors(n: int, primes: list[int]) -> int:
 
 def solve(n: int) -> int:
     """Return sum of amicable numbers in [1..n] range."""
-    # Time complexity: O(sqrt(n) * log(log(sqrt(n))) + n), where
-    #  O(sqrt(n) * log(log(sqrt(n)))) to generate primes;
-    #  O(n * sqrt(n) / log(sqrt(n))) to calculate divisor sums;
-    #  O(n) to find amicable numbers.
-    # Extra space complexity: O(n + sqrt(n) / log(sqrt(n))), where
-    #  O(sqrt(n) / log(sqrt(n))) to hold primes;
-    #  O(n) to hold divisor sums.
+    # Time complexity: O(sqrt(n) * log(log(n)) + n * sqrt(n) / log(n) + n), where
+    #   O(sqrt(n) * log(log(n))) to generate primes;
+    #   O(n * sqrt(n) / log(n)) to calculate divisor sums;
+    #   O(n) to find amicable numbers.
+    # Extra space complexity: O(n + sqrt(n) / log(n)) => O(n), where
+    #   O(sqrt(n) / log(n)) to hold primes;
+    #   O(n) to hold divisor sums.
     primes = gen_primes(math.ceil(math.sqrt(n)) + 1)
 
     proper_divisor_sums = [sum_of_proper_divisors(i, primes) for i in range(n + 1)]
