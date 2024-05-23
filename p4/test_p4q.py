4a3a0a5c45c38e10131bab7e26ecdb8999b2c7e6
Sergey Tinyaev <t_serg@yahoo.com>
2024-05-23 14:57:16 -0500

    Mark to skip some performance-heavy yet optional tests.

--- p4/test_p4q.py
+++ p4/test_p4q.py
@@ -1,3 +1,4 @@
+import pytest
 import p4q
 
 
@@ -5,4 +6,8 @@ def test_solve():
     assert 9009 == p4q.solve(2, 2)
     assert 94149 == p4q.solve(2, 3) == p4q.solve(3, 2)
     assert 906609 == p4q.solve(3, 3)
+
+
+@pytest.mark.skip(reason="Performance benchmark test")
+def test_solve_performance():
     assert 99000099 == p4q.solve(4, 4)
